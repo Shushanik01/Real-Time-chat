1d636bfe3f3b4369a618676ba3fcbf049cbe0bde
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-05-16 16:50:53 +0400

    test google login

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
+import TestApp from './testApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TestApp />
   </StrictMode>,
 )
---
+++ src/testApp.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { signInWithPopup, signOut, onAuthStateChanged } from 'firebase/auth';
+import { auth, googleProvider } from "./firebase/config";
+
+function TestApp() {
+    const handleLogin = async () => {
+
+        try {
+            const result = await signInWithPopup(auth, googleProvider);
+            console.log(result.user.email);
+            
+        }catch(err){
+            throw new Error(err)
+        }
+    };
+
+    const handleLogOut = async()=>{
+       await signOut(auth)
+       console.log('user logged out');
+       
+    };
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, (user)=>{
+            if(user){
+                console.log('User:' ,user.email);
+                
+            } else {
+                console.log('no user');   
+            }
+        });
+        return unsubscribe
+    },[]);
+     return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Real-Time Chat</h1>
+      <button onClick={handleLogin}>Login with Google</button>
+      <button onClick={handleLogOut}>Logout</button>
+    </div>
+  )
+}
+export default TestApp
