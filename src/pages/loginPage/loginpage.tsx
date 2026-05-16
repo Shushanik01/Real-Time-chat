ec70ed90bd483235bdc57fed972c3c2dae4fb501
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-05-17 00:39:01 +0400

    feat: login with google account

--- src/pages/loginPage/loginpage.tsx
+++ src/pages/loginPage/loginpage.tsx
@@ -4,8 +4,64 @@ import logo from '../../assets/logo.png';
 import googleLogo from '../../assets/googleLogo.png';
 import { loginSchema } from '../../schemas/login.schema';
 import type { LoginFormData } from '../../schemas/login.schema';
+import { useState, type FormEvent } from 'react';
+import {  GoogleAuthProvider, signInWithEmailAndPassword } from 'firebase/auth';
+import {auth, googleProvider} from '../../firebase/config';
+import { useNavigate } from 'react-router-dom';
+import { signInWithPopup } from 'firebase/auth';
 
 const Login = () => {
+    const [formData, setFormData] = useState<LoginFormData>({
+        email: '',
+        password: ''
+    });
+
+    const navigate = useNavigate()
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        const result = loginSchema.safeParse(formData);
+
+        if (!result.success) {
+            console.error(result.error.flatten(issue => issue.message).fieldErrors);
+            return;
+        } else{
+           const userLogin = async()=>{
+            try{
+                const response = await signInWithEmailAndPassword(
+                    auth,
+                    formData.email,
+                    formData.password
+                )
+                if(response){
+                    navigate('/chat')
+                }
+            } catch(error){
+                console.log((error as Error).message);   
+            }
+           }
+           userLogin()
+        }
+        
+    };
+
+    const handleGoogleLogin = async()=>{
+        try{
+            const result = await signInWithPopup(auth, googleProvider);
+            const user = await result.user
+            const credentials = GoogleAuthProvider.credentialFromResult(result)
+            const accessToken = credentials?.accessToken
+            if(user.emailVerified){
+                window.alert('Login Successful')
+            const userInfo = [user.displayName, user.email, user.phoneNumber, user.photoURL]
+                console.log(userInfo);
+                navigate('/chat')
+            }
+        } catch(error){
+            throw new Error((error as Error).message)
+        }
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.leftSide}>
@@ -17,7 +73,9 @@ const Login = () => {
                     <h2 className={styles.title}>Login to your Account</h2>
                     <p className={styles.subtitle}>See what is going on with your business</p>
 
-                    <button className={styles.googleBtn}>
+                    <button className={styles.googleBtn}
+                    onClick={handleGoogleLogin}
+                    >
                         <img src={googleLogo} alt="Google logo" className={styles.googleIcon} />
                         Continue with Google
                     </button>
@@ -27,31 +85,44 @@ const Login = () => {
                         <span className={styles.dividerText}>or Sign in with Email</span>
                         <span className={styles.dividerLine} />
                     </div>
+                    <form action="submit"
+                        onSubmit={handleSubmit}
+                    >
+                        <label className={styles.label}>Email</label>
+                        <input
+                            className={styles.input}
+                            type="text"
+                            placeholder="mail@example.com"
+                            value={formData.email}
+                            onChange={(e) => setFormData({
+                                ...formData,
+                                email: e.target.value
+                            })}
+                        />
 
-                    <label className={styles.label}>Email</label>
-                    <input
-                        className={styles.input}
-                        type="text"
-                        placeholder="mail@example.com"
-                    />
+                        <label className={styles.label}>Password</label>
+                        <input
+                            className={styles.input}
+                            type="password"
+                            placeholder="••••••••••"
+                            value={formData.password}
+                            onChange={(e) => setFormData({
+                                ...formData,
+                                password: e.target.value
+                            })}
+                        />
 
-                    <label className={styles.label}>Password</label>
-                    <input
-                        className={styles.input}
-                        type="password"
-                        placeholder="••••••••••"
-                    />
+                        <div className={styles.row}>
+                            <label className={styles.rememberMe}>
+                                <input type="checkbox" className={styles.checkbox} />
+                                Remember Me
+                            </label>
+                            <a href="#" className={styles.forgotPassword}>Forgot Password?</a>
+                        </div>
 
-                    <div className={styles.row}>
-                        <label className={styles.rememberMe}>
-                            <input type="checkbox" className={styles.checkbox} />
-                            Remember Me
-                        </label>
-                        <a href="#" className={styles.forgotPassword}>Forgot Password?</a>
-                    </div>
-
-                    <button className={styles.loginBtn}>Login</button>
-
+                        <button className={styles.loginBtn}
+                        >Login</button>
+                    </form>
                     <p className={styles.registerRow}>
                         Not Registered Yet?&nbsp;
                         <a href="#" className={styles.createAccount}>Create an account</a>
